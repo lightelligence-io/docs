0bf81a43868490bd3fb466147a9c4d4cbe35a9e8
frabbit <frabbit77@googlemail.com>
2019-08-02 16:28:42 +0200

    get initial version working

--- themes/gohugoioTheme_custom/assets/js/docsearch.js
+++ themes/gohugoioTheme_custom/assets/js/docsearch.js
@@ -1,8 +1,8 @@
-var docsearch = require('');
-docsearch({
-  apiKey: '',
-  indexName: '',
-  inputSelector: '',
-  debug: true // Set debug to true if you want to inspect the dropdown
-});
+//var docsearch = require('docsearch.js');
+//docsearch({
+//  apiKey: '',
+//  indexName: '',
+//  inputSelector: '',
+//  debug: true // Set debug to true if you want to inspect the dropdown
+//});
 
