afedb5ecf08c4de4d1d31f52d9b3904c5b73d0e2
Chadha Amara <chadha.amara@db-n.com>
2019-08-09 15:44:15 +0200

    split desktop menu and mobile menu

--- assets/js/menutoggle.js
+++ assets/js/menutoggle.js
@@ -1,8 +1,8 @@
 // Grab any element that has the 'js-toggle' class and add an event listner for the toggleClass function
-  var toggleBtns = document.getElementsByClassName('js-toggle');
-  for (var i = 0; i < toggleBtns.length; i++) {
-    toggleBtns[i].addEventListener('click', toggleClass, false);
-  }
+var toggleBtns = document.getElementsByClassName('js-toggle')
+for (var i = 0; i < toggleBtns.length; i++) {
+    toggleBtns[i].addEventListener('click', toggleClass, false)
+}
 
 var toggleMenuBtn = document.getElementsByClassName('menu-toggle');
 for (var i = 0; i < toggleMenuBtn.length; i++) {
@@ -10,60 +10,25 @@ for (var i = 0; i < toggleMenuBtn.length; i++) {
 }
 
 function toggleClass() {
-  // Define the data target via the dataset "target" (e.g. data-target=".docsmenu")
-  var content = this.dataset.target.split(' ');
-  // Find any menu items that are open
-  var mobileCurrentlyOpen = document.querySelector('.mobilemenu:not(.dn)');
-  var desktopCurrentlyOpen = document.querySelector('.desktopmenu:not(.dn)');
-  var desktopActive = document.querySelector('.desktopmenu:not(.dn)');
+    // Define the data target via the dataset "target" (e.g. data-target=".docsmenu")
+    var content = this.dataset.target.split(' ')
+    // Find any menu items that are open
+    var desktopCurrentlyOpen = document.querySelector('.desktopmenu:not(.dn)')
+    var desktopActive = document.querySelector('.desktopmenu:not(.dn)')
 
-  // Loop through the targets' divs
-  for (var i = 0; i < content.length; i++) {
-    var matches = document.querySelectorAll(content[i]);
-       //for each, if the div has the 'dn' class (which is "display:none;"), remove it, otherwise, add that class
+    // Loop through the targets' divs
+    for (var i = 0; i < content.length; i++) {
+        var matches = document.querySelectorAll(content[i]);
+        //for each, if the div has the 'dn' class (which is "display:none;"), remove it, otherwise, add that class
         [].forEach.call(matches, function(dom) {
-        dom.classList.contains("dn") ?  dom.classList.remove("dn") :  dom.classList.add("dn");
-         return false;
-       });
+            dom.classList.contains('dn') ?
+                dom.classList.remove('dn') :
+                dom.classList.add('dn');
+            return false;
+        });
         // close the currently open menu items
-      if (mobileCurrentlyOpen) mobileCurrentlyOpen.classList.add('dn');
-      if (desktopCurrentlyOpen) desktopCurrentlyOpen.classList.add('dn');
-      if (desktopActive) desktopActive.classList.remove('db');
+        if (desktopCurrentlyOpen) desktopCurrentlyOpen.classList.add('dn')
+        if (desktopActive) desktopActive.classList.remove('db')
+
     }
-}
-
-$('#hamburg-icon').click(function(){
-    $('main#bg-blur, .header-bg-image, #search-input, .container, .header-detail').addClass('bg-blur');
-    $('body').addClass('no-scroll');
-    $('.docsmenu').addClass('scroll-y');
-});
-
-window.addEventListener('mouseup',function(event){
-  var docsmenu = document.getElementById('docsmenu');
-  if(!docsmenu.classList.contains('dn')){
-    if(event.target.id !== 'docsmenu' && !event.target.parentNode.classList.contains('mm')){
-      docsmenu.classList.add(['dn']);
-      $('main#bg-blur, .header-bg-image, #search-input, .container, .header-detail').removeClass('bg-blur');
-      $('body').removeClass('no-scroll');
-    }
-  }
-});
-
-const tableOfContents = $('nav#TableOfContents');
-
-$('.scroll-nav').on('click', () => {
-  tableOfContents.toggleClass('open');
-});
-
-$('nav#TableOfContents a').on('click', () => {
-  setTimeout(function() {
-    tableOfContents.removeClass('open');
-  }, 300);
-});
-
-window.onload = function () {
-  var tabelOfCont = $('#TableOfContents');
-  console.log(tabelOfCont.length);
-  tabelOfCont.length == 0 ?  $('.main-content').removeClass('top-large') : $('.main-content').addClass('top-large');
-
 }
